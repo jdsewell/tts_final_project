d19ea64183584e3808a2c2e712872abe5bc8e6c5
jdsewell <jdsewell@protonmail.com>
2018-10-30 21:00:02 -0400

    Fixed a seeding issue for heroku.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,15 @@
 Book.destroy_all
 Post.destroy_all
 
-Book.create([{ title: 'The Hobbit', author: 'JRR Tolkien', page_length: 310, rating: 5 },
-	{ title: 'Slaughterhouse-Five', author: 'Kurt Vonnegut', page_length: 275, rating: 5 },
-	{ title: 'The Silmarillion', author: 'JRR Tolkien', page_length: 365, rating: 5 }
+Book.create([{ title: 'The Hobbit', author: 'JRR Tolkien', page_length: 310 },
+	{ title: 'Slaughterhouse-Five', author: 'Kurt Vonnegut', page_length: 275 },
+	{ title: 'The Silmarillion', author: 'JRR Tolkien', page_length: 365 }
 ])
 100.times do
 	Book.create(
 		title: Faker::Book.unique.title,
 		author: Faker::Book.author,
-		page_length: rand(1000),
-		rating: rand(5)
+		page_length: rand(1000)
 		)
 end
 
